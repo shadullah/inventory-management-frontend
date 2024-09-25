ef5d593d842a623f93f38bd1386d5c51f27a144a
hexaalif <shadullahsad9@gmail.com>
2024-09-25 17:33:12 +0600

    logout fnctionality done

--- src/Components/Shared/Navbar/Navbar.tsx
+++ src/Components/Shared/Navbar/Navbar.tsx
@@ -1,7 +1,23 @@
+"use client";
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Navbar = () => {
+  const [id, setUserId] = useState<string | null>(null);
+
+  useEffect(() => {
+    if (typeof window !== "undefined") {
+      const userId = localStorage.getItem("id");
+      setUserId(userId);
+    }
+  }, []);
+
+  const handleLogout = () => {
+    localStorage.removeItem("id");
+    setUserId(null);
+    window.location.href = "/authenticate/login";
+  };
+
   return (
     <div className="max-w-[1200px] mx-auto">
       <div className="flex justify-between items-center py-4">
@@ -11,10 +27,23 @@ const Navbar = () => {
           </Link>
         </div>
         <div>
-          <ul>
-            <Link href="/authenticate/login">
-              <li>Login</li>
-            </Link>
+          <ul className="flex space-x-4">
+            {id ? (
+              <>
+                <Link href="/authenticate/login">
+                  <li>Add Product</li>
+                </Link>
+                <li>
+                  <button onClick={handleLogout}>Logout</button>
+                </li>
+              </>
+            ) : (
+              <>
+                <Link href="/authenticate/login">
+                  <li>Login</li>
+                </Link>
+              </>
+            )}
           </ul>
         </div>
       </div>
